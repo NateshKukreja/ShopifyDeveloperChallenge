288646224481133ee2087890eed67c3777e020c4
nateshkukreja <natesh.kukreja@gmail.com>
2018-09-22 18:48:34 -0400

    Creating data files

--- index.py
+++ index.py
@@ -1,22 +1,17 @@
-from flask import Flask
+from flask import *
 import pymongo
 import json
-from flask_pymongo import PyMongo
+from app.schemas import validate_shop
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
-mongo = PyMongo(app)
 
 @app.route('/')
 def home_page():
-    return "hello"
+    return render_template("src/templates/home.html")
 
 @app.route('/api/v1.0', methods=['GET'])
 def dummy_endpoint():
     return json.dumps({'data': 'Server running'}), 200
 
-def init_db():
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["mydatabase"]
-    mycol = mydb["customers"]
-    return mycol
+if __name__ == "__main__":
+    app.run(debug=True)
