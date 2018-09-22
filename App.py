80f4e8953c799bbc29c13aa09e6164c248de9c80
nateshkukreja <natesh.kukreja@gmail.com>
2018-09-21 22:36:11 -0400

    Testing out app routing

--- App.py
+++ App.py
@@ -1,6 +1,30 @@
 from flask import Flask
+import pymongo
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/')
+def return_world():
+    return 'heeeeeeeelloo'
+
+def populateDB():
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["mydatabase"]
+    mycol = mydb["customers"]
+    
+def populateShops():
+    return
+
+def populateOrders():
+    return
+
+def populateProducts():
+    return
+
+def populateLineItems():
+    return
