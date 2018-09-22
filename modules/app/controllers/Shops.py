ccd9fc4271b887dad29bfb4874f9c7555ed3ed7e
nateshkukreja <natesh.kukreja@gmail.com>
2018-09-22 15:26:31 -0400

    Removed logger

--- modules/app/controllers/Shops.py
+++ modules/app/controllers/Shops.py
@@ -6,6 +6,5 @@ from flask_jwt_extended import (create_access_token, create_refresh_token,
 jwt_required, jwt_refresh_token_required, get_jwt_identity)
 from app import app, mongo, flask_bcrypt, jwt
 from app.schemas import validate_shop
-import logger
 
 ROOT_PATH = os.environ.get('ROOT_PATH')
